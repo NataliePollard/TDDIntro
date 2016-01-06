01c357e0f1c9b357435d4904483ae44d5cf5f332
Natalie <natalie.pollard@richmond.edu>
2016-01-05 17:36:59 -0700

    completed work

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+/**
+ * Created by nataliepollard on 1/5/16.
+ */
+public class Account {
+    int balance;
+    public Account(int balance) {
+        this.balance = balance;
+    }
+}
